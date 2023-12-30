7253dea0fc0ff3d2a8baad6fbe51f766de327f8c
윤석규 <greeentea.dev@gmail.com>
2023-12-30 20:00:53 +0900

    style: Apply spotless format

--- app/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/ui/theme/Theme.kt
+++ app/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/ui/theme/Theme.kt
@@ -19,14 +19,14 @@ private val DarkColorScheme =
     darkColorScheme(
         primary = Purple80,
         secondary = PurpleGrey80,
-        tertiary = Pink80
+        tertiary = Pink80,
     )
 
 private val LightColorScheme =
     lightColorScheme(
         primary = Purple40,
         secondary = PurpleGrey40,
-        tertiary = Pink40
+        tertiary = Pink40,
 	/* Other default colors to override
 	background = Color(0xFFFFFBFE),
 	surface = Color(0xFFFFFBFE),
@@ -43,7 +43,7 @@ fun DodamdodamandroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme =
         when {
@@ -67,6 +67,6 @@ fun DodamdodamandroidTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
