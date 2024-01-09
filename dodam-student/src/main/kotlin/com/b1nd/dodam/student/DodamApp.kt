36465c9e753d3253aba73b0bfc77d19e8385c6b6
윤석규 <greeentea.dev@gmail.com>
2024-01-10 03:18:12 +0900

    feat: Create `NavHost` in `MainActvity`

---
+++ dodam-student/src/main/kotlin/com/b1nd/dodam/student/DodamApp.kt
@@ -0,0 +1,19 @@
+package com.b1nd.dodam.student
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.b1nd.dodam.designsystem.theme.DodamdodamandroidTheme
+
+@Composable
+fun DodamApp(navController: NavHostController = rememberNavController()) {
+    DodamdodamandroidTheme {
+        NavHost(
+            navController = navController,
+            startDestination = "" // TODO : Set startDestination
+        ) {
+            // TODO : Add navigation composable
+        }
+    }
+}
