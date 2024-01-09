8d088f3dca8937850dc5e06de9e01f2af972efb4
윤석규 <greeentea.dev@gmail.com>
2024-01-10 02:22:36 +0900

    refactor: Change package name and modify build-logic

---
+++ build-logic/convention/src/main/kotlin/com/b1nd/dodam/primitive/AndroidComposePlugin.kt
@@ -0,0 +1,53 @@
+package com.b1nd.dodam.primitive
+
+import com.b1nd.dodam.dsl.android
+import com.b1nd.dodam.dsl.debugImplementation
+import com.b1nd.dodam.dsl.kotlinOptions
+import com.b1nd.dodam.dsl.library
+import com.b1nd.dodam.dsl.libs
+import com.b1nd.dodam.dsl.version
+import com.b1nd.dodam.dsl.implementation
+import com.b1nd.dodam.dsl.implementationPlatform
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidComposePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            val projectPath = rootProject.file(".").absolutePath
+            android {
+                buildFeatures.compose = true
+                composeOptions {
+                    kotlinCompilerExtensionVersion = libs.version("androidx-compose-compiler")
+                }
+                kotlinOptions {
+                    freeCompilerArgs = freeCompilerArgs + listOf(
+                        "-P",
+                        "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=$projectPath/report/compose-metrics"
+                    )
+                    freeCompilerArgs = freeCompilerArgs + listOf(
+                        "-P",
+                        "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=$projectPath/report/compose-reports"
+                    )
+                }
+            }
+
+            dependencies {
+                implementation(libs.library("androidx-core"))
+                implementationPlatform(libs.library("androidx-compose-bom"))
+                implementation(libs.library("androidx-compose-activity"))
+                implementation(libs.library("androidx-compose-hilt-navigation"))
+                implementation(libs.library("androidx-compose-lifecycle"))
+                implementation(libs.library("androidx-compose-navigation"))
+                implementation(libs.library("androidx-compose-ui"))
+                implementation(libs.library("androidx-compose-ui-foundation"))
+                implementation(libs.library("androidx-compose-material3"))
+                implementation(libs.library("androidx-lifecycle-runtime"))
+                implementation(libs.library("androidx-compose-ui-tooling"))
+                debugImplementation(libs.library("androidx-compose-ui-test-manifest"))
+            }
+        }
+    }
+
+}
