74edf81f855847d58e23d638cfd7829aeb01855e
wsi1212 <wsi1212@dgsw.hs.kr>
2024-01-10 13:56:51 +0900

    feat: Add color foundation

--- core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Color.kt
@@ -9,3 +9,43 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Blue
+val Blue900 = Color(0xFF001A30)
+val Blue800 = Color(0xFF003560)
+val Blue700 = Color(0xFF004F90)
+val Blue600 = Color(0xFF0069C0)
+val Blue500 = Color(0xFF0083F0)
+val Blue400 = Color(0xFF339CF3)
+val Blue300 = Color(0xFF66B5F6)
+val Blue200 = Color(0xFF99CEF9)
+val Blue100 = Color(0xFFCCE6FC)
+val Blue50 = Color(0xFFE2F1FD)
+
+// Red
+val Red900 = Color(0xFF7D0800)
+val Red800 = Color(0xFF9F0A01)
+val Red700 = Color(0xFFBF0A03)
+val Red600 = Color(0xFFDA120D)
+val Red500 = Color(0xFFEF2B2A)
+val Red400 = Color(0xFFFF5454)
+val Red300 = Color(0xFFFF8585)
+val Red200 = Color(0xFFFFBFBF)
+val Red100 = Color(0xFFFFDADB)
+val Red50 = Color(0xFFFFF5F5)
+
+// Gray
+val Gray900 = Color(0xFF222222)
+val Gray800 = Color(0xFF424242)
+val Gray700 = Color(0xFF616161)
+val Gray600 = Color(0xFF757575)
+val Gray500 = Color(0xFF9E9E9E)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray300 = Color(0xFFE0E0E0)
+val Gray200 = Color(0xFFEEEEEE)
+val Gray100 = Color(0xFFF5F5F5)
+val Gray50 = Color(0xFFFAFAFA)
+
+// Black
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
