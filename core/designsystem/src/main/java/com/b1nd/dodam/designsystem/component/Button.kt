a6b0bb1b6fea6ec286ca029636f1cb433e75a819
윤석규 <greeentea.dev@gmail.com>
2024-01-12 01:29:16 +0900

    feat: Add medium button

--- core/designsystem/src/main/java/com/b1nd/dodam/designsystem/component/Button.kt
+++ core/designsystem/src/main/java/com/b1nd/dodam/designsystem/component/Button.kt
@@ -151,6 +151,67 @@ fun DodamLargeButton(
     }
 }
 
+/**
+ * Dodam Medium size button
+ *
+ * @param onClick called when this button is clicked
+ * @param modifier the [Modifier] to be applied to this button
+ * @param text the string content in the center of this button
+ * @param enabled controls the enabled state of this button. When `false`, this component will not
+ * respond to user input, and it will appear visually disabled and disabled to accessibility
+ * services.
+ * @param shape defines the shape of this button's container, border (when [border] is not null),
+ * and shadow (when using [elevation])
+ * @param colors [ButtonColors] that will be used to resolve the colors for this button in different
+ * states. See [ButtonDefaults.buttonColors].
+ * @param elevation [ButtonElevation] used to resolve the elevation for this button in different
+ * states. This controls the size of the shadow below the button. Additionally, when the container
+ * color is [ColorScheme.surface], this controls the amount of primary color applied as an overlay.
+ * See [ButtonElevation.shadowElevation] and [ButtonElevation.tonalElevation].
+ * @param border the border to draw around the container of this button
+ * @param contentPadding the spacing values to apply internally between the container and the
+ * content
+ * @param interactionSource the [MutableInteractionSource] representing the stream of [Interaction]s
+ * for this button. You can create and pass in your own `remember`ed instance to observe
+ * [Interaction]s and customize the appearance / behavior of this button in different states.
+ */
+@Composable
+fun DodamMediumButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    text: String,
+    enabled: Boolean = true,
+    shape: Shape = RoundedCornerShape(10.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        disabledContainerColor = MaterialTheme.colorScheme.primary.copy(0.3f),
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary,
+    ),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 16.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .bounceClick(interactionSource),
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
 @Preview
 @Composable
 private fun DodamFullWidthButtonPreview() {
@@ -167,6 +228,17 @@ private fun DodamFullWidthButtonPreview() {
 private fun DodamLargeButtonPreview() {
     DodamTheme {
         DodamLargeButton(
+            onClick = {},
+            text = "버튼"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DodamMediumButtonPreview() {
+    DodamTheme {
+        DodamMediumButton(
             onClick = {},
             text = "버튼"
         )
