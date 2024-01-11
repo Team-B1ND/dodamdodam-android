794e341cfefab4a05a988dadc3f73720bc5a1d72
wsi1212 <wsi1212@dgsw.hs.kr>
2024-01-11 16:44:30 +0900

    remove: Remove `Roboto` typography

--- core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Typography.kt
@@ -8,16 +8,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.b1nd.dodam.designsystem.R
 
-// Set of Material typography styles to start with
-
 val NotoSansKrFontFamily = FontFamily(
     Font(R.font.notosanskr_regular, FontWeight.Normal),
     Font(R.font.notosanskr_medium, FontWeight.Medium),
     Font(R.font.notosanskr_bold, FontWeight.Bold),
 )
 
-val NotoSansKR =
-    Typography(
+val NotoSansKR = Typography(
         titleLarge = TextStyle(
             fontFamily = NotoSansKrFontFamily,
             fontWeight = FontWeight.Bold,
@@ -82,69 +79,3 @@ val NotoSansKR =
             letterSpacing = 0.25.sp,
         ),
     )
-
-val Roboto = Typography(
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.35.sp,
-    ),
-    titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 18.sp,
-        lineHeight = 23.sp,
-        letterSpacing = 0.38.sp,
-    ),
-    titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 16.sp,
-        lineHeight = 21.sp,
-        letterSpacing = 0.15.sp,
-    ),
-    labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.1.sp,
-    ),
-    labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 12.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp,
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp,
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 18.sp,
-        lineHeight = 26.sp,
-        letterSpacing = 0.15.sp,
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-    ),
-    bodySmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.25.sp,
-    ),
-)
