831f1d8e0dfa08ff700b40baf2c8b200bd78c968
wsi1212 <wsi1212@dgsw.hs.kr>
2024-01-11 11:09:49 +0900

    remove: Remove unused default colors

--- core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/b1nd/dodam/designsystem/theme/Color.kt
@@ -2,14 +2,6 @@ package com.b1nd.dodam.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Blue
 val Blue900 = Color(0xFF001A30)
 val Blue800 = Color(0xFF003560)
