5b56e2b0cd93b533dc09e6c06296bfed11637dc7
wsi1212 <wsi1212@dgsw.hs.kr>
2024-02-04 02:52:08 +0900

    feat: Add register navigation

---
+++ feature/register/src/main/java/kr/hs/dgsw/register/AuthScreen.kt
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RegisterScreenPreview() {
+    RegisterScreen()
+}
---
+++ feature/register/src/main/java/kr/hs/dgsw/register/navigation/AuthNavigation.kt
@@ -0,0 +1,19 @@
+package kr.hs.dgsw.register.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+
+const val AUTH_ROUTE = "auth"
+
+fun NavController.navigateToAuth(navOptions: NavOptions?) = navigate(AUTH_ROUTE, navOptions)
+
+fun NavGraphBuilder.authScreen(onRegisterClick: () -> Unit, onBackClick: () -> Unit) {
+    composable(route = AUTH_ROUTE) {
+        authScreen(
+            onRegisterClick = onRegisterClick,
+            onBackClick = onBackClick,
+        )
+    }
+}
