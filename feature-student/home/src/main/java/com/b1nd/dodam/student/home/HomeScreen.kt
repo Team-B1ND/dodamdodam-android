c3bc5dc384b68ce4549a7c0512681e6224e4b912
윤석규 <greeentea.dev@gmail.com>
2024-02-14 17:07:57 +0900

    feat: Create home module

---
+++ feature-student/home/src/main/java/com/b1nd/dodam/student/home/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.b1nd.dodam.student.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        
+    }
+}
