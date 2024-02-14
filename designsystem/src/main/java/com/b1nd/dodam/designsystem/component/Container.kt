cf4adf71d008c33e9a1423cd20fe20de3a6dd740
윤석규 <greeentea.dev@gmail.com>
2024-02-14 17:06:36 +0900

    feat: Create container component

---
+++ designsystem/src/main/java/com/b1nd/dodam/designsystem/component/Container.kt
@@ -0,0 +1,95 @@
+package com.b1nd.dodam.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.designsystem.animation.NoInteractionSource
+import com.b1nd.dodam.designsystem.icons.ChevronRight
+
+@Composable
+fun DodamContainer(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    title: String,
+    showNextButton: Boolean = false,
+    onNextClick: (() -> Unit)? = null,
+    content: @Composable () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = RoundedCornerShape(20.dp)
+            )
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(100),
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(20.dp),
+                    imageVector = icon,
+                    contentDescription = "icon",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            if (showNextButton) {
+                Icon(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .clickable(
+                            interactionSource = NoInteractionSource(),
+                            indication = null,
+                            onClick = onNextClick!!
+                        ),
+                    imageVector = ChevronRight,
+                    contentDescription = "next",
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+        
+        content()
+    }
+}
