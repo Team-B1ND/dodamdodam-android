c8d006cbec07736ee29caa44b55129267d0de0d2
윤석규 <greeentea.dev@gmail.com>
2024-02-25 22:07:40 +0900

    refactor: Change from kotlin library to android library

--- data/login/build.gradle.kts
+++ data/login/build.gradle.kts
@@ -1,11 +1,23 @@
+import com.b1nd.dodam.dsl.android
+
 plugins {
-    alias(libs.plugins.dodam.kotlin)
-    alias(libs.plugins.dodam.hilt)
+    alias(libs.plugins.dodam.android)
+    alias(libs.plugins.dodam.android.kotlin)
+    alias(libs.plugins.dodam.android.hilt)
+}
+
+android {
+    android {
+        namespace = "com.b1nd.dodam.data.login"
+
+        defaultConfig {
+            consumerProguardFiles("consumer-rules.pro")
+        }
+    }
 }
 
 dependencies {
     api(projects.model)
     implementation(projects.common)
-    implementation(projects.network.core)
     implementation(projects.network.login)
 }
