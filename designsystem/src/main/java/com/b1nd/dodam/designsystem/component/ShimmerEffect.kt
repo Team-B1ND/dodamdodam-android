f5681bd731654488aafe37defc7e735f7ee48da0
윤석규 <greeentea.dev@gmail.com>
2024-03-01 01:44:06 +0900

    feat: Add shimmer effect when loading

--- designsystem/src/main/java/com/b1nd/dodam/designsystem/component/ShimmerEffect.kt
+++ designsystem/src/main/java/com/b1nd/dodam/designsystem/component/ShimmerEffect.kt
@@ -11,32 +11,39 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun shimmerBrush(showShimmer: Boolean = true, targetValue: Float = 1000f): Brush {
+fun shimmerEffect(showShimmer: Boolean = true, targetValue: Float = 1000f): Brush {
     return if (showShimmer) {
+        // Colors for the shimmer effect
         val shimmerColors = listOf(
             Color.LightGray.copy(alpha = 0.6f),
             Color.LightGray.copy(alpha = 0.2f),
             Color.LightGray.copy(alpha = 0.6f),
         )
 
-        val transition = rememberInfiniteTransition()
+        // Start the animation transition
+        val transition = rememberInfiniteTransition(label = "")
         val translateAnimation = transition.animateFloat(
             initialValue = 0f,
             targetValue = targetValue,
             animationSpec = infiniteRepeatable(
-                animation = tween(800), repeatMode = RepeatMode.Reverse
-            )
+                animation = tween(800),
+                repeatMode = RepeatMode.Reverse,
+            ),
+            label = "",
         )
+
+        // Return a linear gradient brush
         Brush.linearGradient(
             colors = shimmerColors,
             start = Offset.Zero,
-            end = Offset(x = translateAnimation.value, y = translateAnimation.value)
+            end = Offset(x = translateAnimation.value, y = translateAnimation.value),
         )
     } else {
+        // If shimmer is turned off, return a transparent brush
         Brush.linearGradient(
             colors = listOf(Color.Transparent, Color.Transparent),
             start = Offset.Zero,
-            end = Offset.Zero
+            end = Offset.Zero,
         )
     }
 }
