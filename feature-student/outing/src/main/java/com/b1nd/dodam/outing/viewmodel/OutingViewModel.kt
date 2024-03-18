c30d8df3111e070224d51ae6f05064ba43649dfb
wsi1212 <wsi1212@dgsw.hs.kr>
2024-03-18 16:25:53 +0900

    feat: Add outing business logic

---
+++ feature-student/outing/src/main/java/com/b1nd/dodam/outing/viewmodel/OutingViewModel.kt
@@ -0,0 +1,66 @@
+package com.b1nd.dodam.outing.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.b1nd.dodam.common.result.Result
+import com.b1nd.dodam.data.outing.OutingRepository
+import com.b1nd.dodam.outing.OutingUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OutingViewModel @Inject constructor(
+    private val outingRepository: OutingRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(OutingUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _event = MutableSharedFlow<Event>()
+    val event = _event.asSharedFlow()
+
+    init {
+        getMyOuting()
+    }
+
+    fun getMyOuting() {
+        viewModelScope.launch {
+            outingRepository.getMyOut().collect { result ->
+                _uiState.update {
+                    when (result) {
+                        is Result.Error -> {
+                            _event.emit(Event.Error(result.error.message.toString()))
+                            Log.e("ERROR", result.error.message.toString())
+                            it.copy(
+                                isLoading = false,
+                            )
+                        }
+                        Result.Loading -> {
+                            it.copy(
+                                isLoading = true,
+                            )
+                        }
+                        is Result.Success -> {
+
+                            it.copy(
+                                isLoading = false,
+                                outings = result.data
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+sealed interface Event {
+    data class Error(val message: String) : Event
+}
+
