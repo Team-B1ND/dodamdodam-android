8c95b1d73d202017e8f83178fd8e88d1095afb33
윤석규 <greeentea.dev@gmail.com>
2024-03-25 21:57:54 +0900

    refactor: Apply dodam design system to login screen

--- feature/login/src/main/java/com/b1nd/dodam/login/viewmodel/LoginViewModel.kt
+++ feature/login/src/main/java/com/b1nd/dodam/login/viewmodel/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.b1nd.dodam.login.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.b1nd.dodam.common.exception.ForbiddenException
@@ -9,17 +8,25 @@ import com.b1nd.dodam.common.exception.UnauthorizedException
 import com.b1nd.dodam.common.result.Result
 import com.b1nd.dodam.data.login.repository.LoginRepository
 import com.b1nd.dodam.datastore.repository.DatastoreRepository
+import com.b1nd.dodam.login.model.LoginUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginRepository: LoginRepository,
     private val datastoreRepository: DatastoreRepository,
 ) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState = _uiState.asStateFlow()
+
     private val _event = MutableSharedFlow<Event>()
     val event = _event.asSharedFlow()
 
@@ -27,6 +34,11 @@ class LoginViewModel @Inject constructor(
         loginRepository.login(id, pw).collect { result ->
             when (result) {
                 is Result.Success -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false
+                        )
+                    }
                     datastoreRepository.saveUser(
                         id = id,
                         pw = pw,
@@ -37,24 +49,31 @@ class LoginViewModel @Inject constructor(
                 }
 
                 is Result.Error -> {
+                    _uiState.update { it.copy(isLoading = false) }
                     when (result.error) {
                         is ForbiddenException -> {
-                            _event.emit(Event.Error("아직 계정이 승인되지 않았어요. 승인을 기다려주세요."))
+                            _event.emit(Event.ShowDialog)
+                            _uiState.update { it.copy(error = "아직 계정이 승인되지 않았어요") }
                         }
                         is NotFoundException -> {
-                            _event.emit(Event.Error("아이디를 확인해주세요."))
+                            _event.emit(Event.CheckId("아이디를 확인해주세요."))
                         }
                         is UnauthorizedException -> {
-                            _event.emit(Event.Error("비밀번호를 확인해주세요."))
+                            _event.emit(Event.CheckPw("비밀번호를 확인해주세요."))
                         }
                         else -> {
-                            Log.e("login: ", "stauts: ${result.error.javaClass.simpleName}")
-                            _event.emit(Event.Error("알 수 없는 오류가 발생했습니다. 잠시만 기다려주세요."))
+                            _event.emit(Event.ShowDialog)
+                            _uiState.update { it.copy(error =  "알 수 없는 오류가 발생했어요") }
                         }
                     }
+
                 }
 
-                is Result.Loading -> {}
+                is Result.Loading -> {
+                    _uiState.update {
+                        it.copy(isLoading = true)
+                    }
+                }
             }
         }
     }
@@ -62,5 +81,7 @@ class LoginViewModel @Inject constructor(
 
 sealed interface Event {
     data object NavigateToMain : Event
-    data class Error(val message: String) : Event
+    data object ShowDialog : Event
+    data class CheckId(val message: String) : Event
+    data class CheckPw(val message: String) : Event
 }
