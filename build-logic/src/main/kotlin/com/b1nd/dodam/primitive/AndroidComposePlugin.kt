a2d05c3697c17e10c36e9ad2fe1c80d1a6eba414
윤석규 <greeentea.dev@gmail.com>
2024-03-28 15:15:20 +0900

    feat: Add compose investigator to trace recomposition

--- build-logic/src/main/kotlin/com/b1nd/dodam/primitive/AndroidComposePlugin.kt
+++ build-logic/src/main/kotlin/com/b1nd/dodam/primitive/AndroidComposePlugin.kt
@@ -1,6 +1,7 @@
 package com.b1nd.dodam.primitive
 
 import com.b1nd.dodam.dsl.android
+import com.b1nd.dodam.dsl.androidTestImplementation
 import com.b1nd.dodam.dsl.debugImplementation
 import com.b1nd.dodam.dsl.kotlinOptions
 import com.b1nd.dodam.dsl.library
@@ -8,13 +9,18 @@ import com.b1nd.dodam.dsl.libs
 import com.b1nd.dodam.dsl.version
 import com.b1nd.dodam.dsl.implementation
 import com.b1nd.dodam.dsl.implementationPlatform
+import com.b1nd.dodam.dsl.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidComposePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            with(pluginManager) {
+                apply("land.sungbin.composeinvestigator")
+            }
             val projectPath = rootProject.file(".").absolutePath
             android {
                 buildFeatures.compose = true
