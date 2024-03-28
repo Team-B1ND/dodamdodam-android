592c85e27a4d035e7d4e277f08334d9f0acc5140
윤석규 <greeentea.dev@gmail.com>
2024-03-28 16:17:06 +0900

    fix: Fix merge conflict

--- ui-test-main/src/main/kotlin/com/b1nd/dodam/test/MainActivity.kt
+++ ui-test-main/src/main/kotlin/com/b1nd/dodam/test/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
 import com.b1nd.dodam.designsystem.theme.DodamTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
+@ExperimentalMaterial3Api
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
