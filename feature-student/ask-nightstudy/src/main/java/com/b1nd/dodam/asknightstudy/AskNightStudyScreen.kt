1b437ff600c659b2bada709e78255de959b05161
윤석규 <greeentea.dev@gmail.com>
2024-03-31 19:41:07 +0900

    feat: Make can scroll when keyboard appears

--- feature-student/ask-nightstudy/src/main/java/com/b1nd/dodam/asknightstudy/AskNightStudyScreen.kt
+++ feature-student/ask-nightstudy/src/main/java/com/b1nd/dodam/asknightstudy/AskNightStudyScreen.kt
@@ -1,5 +1,6 @@
 package com.b1nd.dodam.asknightstudy
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,9 +8,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -37,6 +40,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -229,7 +233,8 @@ internal fun AskNightStudyScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .verticalScroll(scrollState)
-                    .padding(horizontal = 16.dp),
+                    .padding(horizontal = 16.dp)
+                    .imePadding()
             ) {
                 Column(
                     modifier = Modifier
@@ -380,6 +385,8 @@ internal fun AskNightStudyScreen(
                         )
                     }
                 }
+
+                Spacer(modifier = Modifier.height(100.dp))
             }
 
             DodamCTAButton(
