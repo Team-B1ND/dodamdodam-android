2bf9c31714a5f358be14d728d7b4ac172bebd734
윤석규 <greeentea.dev@gmail.com>
2024-04-02 03:31:11 +0900

    feat: Create point business logic

---
+++ data/point/src/main/java/com/b1nd/dodam/data/outing/model/ScoreType.kt
@@ -0,0 +1,16 @@
+package com.b1nd.dodam.data.outing.model
+
+import com.b1nd.dodam.network.point.model.NetworkPointType
+import com.b1nd.dodam.network.point.model.NetworkScoreType
+
+enum class ScoreType {
+    BONUS,
+    MINUS,
+    OFFSET,
+}
+
+internal fun NetworkScoreType.toModel() = when (this) {
+    NetworkScoreType.BONUS -> ScoreType.BONUS
+    NetworkScoreType.MINUS -> ScoreType.MINUS
+    NetworkScoreType.OFFSET -> ScoreType.OFFSET
+}
