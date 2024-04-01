b1694ef90dbe3b719a87ccf11f9f0e78ff02b021
wsi1212 <wsi1212@dgsw.hs.kr>
2024-04-01 16:16:09 +0900

    feat: Add bus data module

---
+++ data/bus/src/main/java/com/b1nd/dodam/bus/model/Bus.kt
@@ -0,0 +1,21 @@
+package com.b1nd.dodam.bus.model
+
+data class Bus(
+    val applyCount: Int,
+    val busName: String,
+    val description: String,
+    val id: Int,
+    val leaveTime: String,
+    val peopleLimit: Int,
+    val timeRequired: String
+)
+
+fun BusResponse.toModel() = Bus(
+    applyCount = this.applyCount,
+    busName = this.busName,
+    description = this.description,
+    id = this.id,
+    leaveTime = this.leaveTime,
+    peopleLimit = this.peopleLimit,
+    timeRequired = this.timeRequired,
+)
