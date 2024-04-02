05a626374bbcb2e48c8b0adb74cf68b2bb01e91a
윤석규 <greeentea.dev@gmail.com>
2024-04-02 14:19:16 +0900

    feat: Show dialog when fail to get points

--- feature-student/point/src/main/java/com/b1nd/dodam/student/point/PointViewModel.kt
+++ feature-student/point/src/main/java/com/b1nd/dodam/student/point/PointViewModel.kt
@@ -5,25 +5,29 @@ import androidx.lifecycle.viewModelScope
 import com.b1nd.dodam.common.result.Result
 import com.b1nd.dodam.data.point.PointRepository
 import com.b1nd.dodam.data.point.model.Point
-import com.b1nd.dodam.data.point.model.PointReason
 import com.b1nd.dodam.data.point.model.PointType
 import com.b1nd.dodam.data.point.model.ScoreType
 import com.b1nd.dodam.student.point.model.PointUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class PointViewModel @Inject constructor(
-    private val pointRepository: PointRepository
+    private val pointRepository: PointRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(PointUiState())
     val uiState = _uiState.asStateFlow()
+
+    private val _event = MutableSharedFlow<Event>()
+    val event = _event.asSharedFlow()
 
     init {
         viewModelScope.launch {
@@ -35,26 +39,26 @@ class PointViewModel @Inject constructor(
                                 isLoading = false,
                                 schoolPoint = result.data.getScore(
                                     ScoreType.MINUS,
-                                    PointType.SCHOOL
+                                    PointType.SCHOOL,
                                 ) - result.data.getScore(
                                     ScoreType.OFFSET,
-                                    PointType.SCHOOL
+                                    PointType.SCHOOL,
                                 ) to result.data.getScore(
                                     ScoreType.BONUS,
-                                    PointType.SCHOOL
+                                    PointType.SCHOOL,
                                 ),
                                 dormitoryPoint = result.data.getScore(
                                     ScoreType.MINUS,
-                                    PointType.DORMITORY
+                                    PointType.DORMITORY,
                                 ) - result.data.getScore(
                                     ScoreType.OFFSET,
-                                    PointType.DORMITORY
+                                    PointType.DORMITORY,
                                 ) to result.data.getScore(
                                     ScoreType.BONUS,
-                                    PointType.DORMITORY
+                                    PointType.DORMITORY,
                                 ),
                                 schoolPointReasons = result.data.getPointReason(PointType.SCHOOL),
-                                dormitoryPointReasons = result.data.getPointReason(PointType.DORMITORY)
+                                dormitoryPointReasons = result.data.getPointReason(PointType.DORMITORY),
                             )
                         }
                     }
@@ -69,19 +73,22 @@ class PointViewModel @Inject constructor(
                         _uiState.update {
                             it.copy(isLoading = false)
                         }
+                        _event.emit(Event.ShowDialog)
                     }
                 }
             }
         }
     }
 
-    private fun List<Point>.getScore(scoreType: ScoreType, pointType: PointType): Int =
-        this.filter { it.reason.scoreType == scoreType }
-            .filter { it.pointType == pointType }
-            .sumOf { it.reason.score }
+    private fun List<Point>.getScore(scoreType: ScoreType, pointType: PointType): Int = this.filter { it.reason.scoreType == scoreType }
+        .filter { it.pointType == pointType }
+        .sumOf { it.reason.score }
 
-    private fun List<Point>.getPointReason(pointType: PointType): ImmutableList<Point> =
-        this.filter {
-            it.pointType == pointType
-        }.toImmutableList()
+    private fun List<Point>.getPointReason(pointType: PointType): ImmutableList<Point> = this.filter {
+        it.pointType == pointType
+    }.toImmutableList()
+}
+
+sealed interface Event {
+    data object ShowDialog : Event
 }
