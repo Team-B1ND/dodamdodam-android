6be28e312b29e8f356faaf1793b4ec67c9dfcc17
wsi1212 <wsi1212@dgsw.hs.kr>
2024-04-02 09:06:04 +0900

    refactor: Reflects code reviews

--- data/bus/src/main/java/com/b1nd/dodam/bus/repository/BusRepository.kt
+++ data/bus/src/main/java/com/b1nd/dodam/bus/repository/BusRepository.kt
@@ -2,10 +2,11 @@ package com.b1nd.dodam.bus.repository
 
 import com.b1nd.dodam.bus.model.Bus
 import com.b1nd.dodam.common.result.Result
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.Flow
 
 interface BusRepository {
-    fun getBusList(): Flow<Result<List<Bus>>>
+    fun getBusList(): Flow<Result<ImmutableList<Bus>>>
     fun applyBus(id: Int): Flow<Result<Unit>>
     fun deleteBus(id: Int): Flow<Result<Unit>>
     fun updateBus(id: Int): Flow<Result<Unit>>
