a95846d70b46ffcc71dcb47cdd7b246e0cd0fe78
wsi1212 <wsi1212@dgsw.hs.kr>
2024-04-04 10:57:24 +0900

    fix: Fix ask wakeup song error http status code

--- network/core/src/main/java/com/b1nd/dodam/network/core/util/ApiRequest.kt
+++ network/core/src/main/java/com/b1nd/dodam/network/core/util/ApiRequest.kt
@@ -4,8 +4,8 @@ import com.b1nd.dodam.common.exception.BadRequestException
 import com.b1nd.dodam.common.exception.ConflictException
 import com.b1nd.dodam.common.exception.DataNotFoundException
 import com.b1nd.dodam.common.exception.ForbiddenException
-import com.b1nd.dodam.common.exception.IMUsedException
 import com.b1nd.dodam.common.exception.InternalServerException
+import com.b1nd.dodam.common.exception.LockedException
 import com.b1nd.dodam.common.exception.NotFoundException
 import com.b1nd.dodam.common.exception.TooManyRequestsException
 import com.b1nd.dodam.common.exception.UnauthorizedException
@@ -16,12 +16,12 @@ suspend inline fun <T> safeRequest(crossinline request: suspend () -> Response<T
     val response = request()
     return when (response.status) {
         200, 201, 204 -> response.data ?: throw DataNotFoundException(response.message)
-        226 -> throw IMUsedException(response.message)
         400 -> throw BadRequestException(response.message)
         401 -> throw UnauthorizedException(response.message)
         403 -> throw ForbiddenException(response.message)
         404 -> throw NotFoundException(response.message)
         409 -> throw ConflictException(response.message)
+        423 -> throw LockedException(response.message)
         429 -> throw TooManyRequestsException(response.message)
         500 -> throw InternalServerException(response.message, response.status)
         else -> throw Exception(response.message)
@@ -32,12 +32,12 @@ suspend inline fun defaultSafeRequest(crossinline request: suspend () -> Default
     val response = request()
     return when (response.status) {
         200, 201, 204 -> Unit
-        226 -> throw IMUsedException(response.message)
         400 -> throw BadRequestException(response.message)
         401 -> throw UnauthorizedException(response.message)
         403 -> throw ForbiddenException(response.message)
         404 -> throw NotFoundException(response.message)
         409 -> throw ConflictException(response.message)
+        423 -> throw LockedException(response.message)
         429 -> throw TooManyRequestsException(response.message)
         500 -> throw InternalServerException(response.message, response.status)
         else -> throw Exception(response.message)
