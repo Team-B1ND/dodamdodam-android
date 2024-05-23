14f1a33c61a1c31e1d15c1484a05d353ce0ecd40
8954sood <8954sood@naver.com>
2024-05-23 16:29:53 +0900

    feat: Add Google App Update

--- dodam-student/build.gradle.kts
+++ dodam-student/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
+    implementation(libs.google.app.update)
 }
