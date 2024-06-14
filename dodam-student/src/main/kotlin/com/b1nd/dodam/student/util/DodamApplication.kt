46585d89c385e18d142cf24ecf478dd530783ab9
8954sood <8954sood@naver.com>
2024-06-14 09:22:31 +0900

    feat: Create DodamApplication Koin Setting

--- dodam-student/src/main/kotlin/com/b1nd/dodam/student/util/DodamApplication.kt
+++ dodam-student/src/main/kotlin/com/b1nd/dodam/student/util/DodamApplication.kt
@@ -2,6 +2,23 @@ package com.b1nd.dodam.student.util
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
 
 @HiltAndroidApp
-class DodamApplication : Application()
+class DodamApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@DodamApplication)
+            modules()
+        }
+    }
+}
