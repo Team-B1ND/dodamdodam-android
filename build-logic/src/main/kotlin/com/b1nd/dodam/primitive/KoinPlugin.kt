9579be72f232827108dc08b2f9b1cbddb8fd0729
8954sood <8954sood@naver.com>
2024-06-14 09:20:14 +0900

    feat: Create Koin Plugin

---
+++ build-logic/src/main/kotlin/com/b1nd/dodam/primitive/KoinPlugin.kt
@@ -0,0 +1,22 @@
+package com.b1nd.dodam.primitive
+
+import com.b1nd.dodam.dsl.implementation
+import com.b1nd.dodam.dsl.library
+import com.b1nd.dodam.dsl.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class KoinPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                dependencies {
+                    implementation(libs.library("koin-android"))
+                    implementation(libs.library("koin-core"))
+                }
+            }
+        }
+    }
+}
