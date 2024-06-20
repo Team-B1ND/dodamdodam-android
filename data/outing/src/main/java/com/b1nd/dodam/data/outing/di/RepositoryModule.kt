e0ba6c878ff53a03af11c3df6d664ef05b6dcdbb
8954sood <8954sood@naver.com>
2024-06-20 23:09:04 +0900

    style: Apply Spotless Format

--- data/outing/src/main/java/com/b1nd/dodam/data/outing/di/RepositoryModule.kt
+++ data/outing/src/main/java/com/b1nd/dodam/data/outing/di/RepositoryModule.kt
@@ -3,10 +3,6 @@ package com.b1nd.dodam.data.outing.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.outing.OutingRepository
 import com.b1nd.dodam.data.outing.repository.OutingRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
--- feature-student/outing/src/main/java/com/b1nd/dodam/outing/viewmodel/OutViewModel.kt
+++ feature-student/outing/src/main/java/com/b1nd/dodam/outing/viewmodel/OutViewModel.kt
@@ -18,8 +18,7 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 @HiltViewModel
-class OutingViewModel @Inject constructor(
-) : ViewModel(), KoinComponent {
+class OutingViewModel @Inject constructor() : ViewModel(), KoinComponent {
 
     private val outingRepository: OutingRepository by inject()
 
