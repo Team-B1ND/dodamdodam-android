cfd876551a4980277eb2ab7b9a249ed83e25c7e1
8954sood <8954sood@naver.com>
2024-06-25 18:19:53 +0900

    style: Apply Spotless Format

--- data/bus/src/main/java/com/b1nd/dodam/bus/di/RepositoryModule.kt
+++ data/bus/src/main/java/com/b1nd/dodam/bus/di/RepositoryModule.kt
@@ -3,13 +3,8 @@ package com.b1nd.dodam.bus.di
 import com.b1nd.dodam.bus.repository.BusRepository
 import com.b1nd.dodam.bus.repositoryimpl.BusRepositoryImpl
 import com.b1nd.dodam.common.DispatcherType
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val busRepositoryModule = module {
     single<BusRepository> {
