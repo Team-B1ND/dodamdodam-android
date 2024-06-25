afb013dbd1b7404943627b1c807b394ed1d5c503
8954sood <8954sood@naver.com>
2024-06-25 18:18:21 +0900

    refactor: Bus Di libaray hilt to Koin

--- data/bus/src/main/java/com/b1nd/dodam/bus/di/RepositoryModule.kt
+++ data/bus/src/main/java/com/b1nd/dodam/bus/di/RepositoryModule.kt
@@ -2,17 +2,17 @@ package com.b1nd.dodam.bus.di
 
 import com.b1nd.dodam.bus.repository.BusRepository
 import com.b1nd.dodam.bus.repositoryimpl.BusRepositoryImpl
+import com.b1nd.dodam.common.DispatcherType
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-interface RepositoryModule {
-
-    @Binds
-    @Singleton
-    fun bindsBusRepository(busRepositoryImpl: BusRepositoryImpl): BusRepository
+val busRepositoryModule = module {
+    single<BusRepository> {
+        BusRepositoryImpl(get(), get(named(DispatcherType.IO)))
+    }
 }
--- network/bus/src/main/java/com/b1nd/dodam/bus/di/DataSourceModule.kt
+++ network/bus/src/main/java/com/b1nd/dodam/bus/di/DataSourceModule.kt
@@ -6,12 +6,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.dsl.module
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-internal interface DataSourceModule {
-    @Binds
-    @Singleton
-    fun provideBusDataSource(busService: BusService): BusDataSource
+val busDataSourceModule = module {
+    single<BusDataSource> {
+        BusService(get())
+    }
 }
