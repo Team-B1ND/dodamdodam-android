2c6fb6dc4c21a0b473d1b436dd010f76b5feee19
8954sood <8954sood@naver.com>
2024-06-26 09:33:34 +0900

    refactor: Schedule Di library Hilt to Koin

--- dodam-student/build.gradle.kts
+++ dodam-student/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     implementation(projects.network.wakeupSong)
     implementation(projects.data.outing)
     implementation(projects.network.outing)
+    implementation(projects.data.schedule)
+    implementation(projects.network.schedule)
 }
