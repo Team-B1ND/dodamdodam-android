2a39e9a9967e9862ac62781a2fbabbe45451459e
8954sood <8954sood@naver.com>
2024-06-26 09:34:22 +0900

    style: Apply Spotless Format

--- data/schedule/src/main/java/com/b1nd/dodam/data/schedule/di/RepositoryModule.kt
+++ data/schedule/src/main/java/com/b1nd/dodam/data/schedule/di/RepositoryModule.kt
@@ -3,10 +3,6 @@ package com.b1nd.dodam.data.schedule.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.schedule.ScheduleRepository
 import com.b1nd.dodam.data.schedule.repository.ScheduleRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
--- network/schedule/src/main/java/com/b1nd/dodam/network/schedule/di/DatasourceModule.kt
+++ network/schedule/src/main/java/com/b1nd/dodam/network/schedule/di/DatasourceModule.kt
@@ -2,12 +2,7 @@ package com.b1nd.dodam.network.schedule.di
 
 import com.b1nd.dodam.network.schedule.api.ScheduleService
 import com.b1nd.dodam.network.schedule.datasource.ScheduleDataSource
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val scheduleDatasourceModule = module {
     single<ScheduleDataSource> {
