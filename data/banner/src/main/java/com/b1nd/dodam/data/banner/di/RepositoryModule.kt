1c4b940e2d123f7b3161e51351f99a7282109b9d
8954sood <8954sood@naver.com>
2024-06-26 09:03:42 +0900

    style: Apply Spotless Format

--- data/banner/src/main/java/com/b1nd/dodam/data/banner/di/RepositoryModule.kt
+++ data/banner/src/main/java/com/b1nd/dodam/data/banner/di/RepositoryModule.kt
@@ -3,13 +3,8 @@ package com.b1nd.dodam.data.banner.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.banner.BannerRepository
 import com.b1nd.dodam.data.banner.repository.BannerRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val bannerRepositoryModule = module {
     single<BannerRepository> {
