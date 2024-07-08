91fcb5f49636044f9dbc6d037928128eff22cd97
8954sood <8954sood@naver.com>
2024-07-08 16:59:07 +0900

    refactor: Register Di library Hilt to Koin

--- data/register/src/main/java/com/b1nd/dodam/register/di/RepositoryModule.kt
+++ data/register/src/main/java/com/b1nd/dodam/register/di/RepositoryModule.kt
@@ -1,17 +1,18 @@
 package com.b1nd.dodam.register.di
 
+import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.register.repository.RegisterRepository
 import com.b1nd.dodam.register.repositoryImpl.RegisterRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-internal interface RepositoryModule {
-    @Binds
-    @Singleton
-    fun bindsRegisterRepository(registerRepositoryImpl: RegisterRepositoryImpl): RegisterRepository
+val registerRepositoryModule = module {
+    single<RegisterRepository> {
+        RegisterRepositoryImpl(get(), get(named(DispatcherType.IO)))
+    }
 }
