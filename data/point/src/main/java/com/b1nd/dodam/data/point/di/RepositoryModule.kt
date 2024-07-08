3d122b77d44ed372e8431c76ca41320d6ce23e79
8954sood <8954sood@naver.com>
2024-07-08 13:34:45 +0900

    refactor: Point Di library Hilt to Koin

--- data/point/src/main/java/com/b1nd/dodam/data/point/di/RepositoryModule.kt
+++ data/point/src/main/java/com/b1nd/dodam/data/point/di/RepositoryModule.kt
@@ -1,16 +1,17 @@
 package com.b1nd.dodam.data.point.di
 
+import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.point.PointRepository
 import com.b1nd.dodam.data.point.repository.PointRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
-@Module
-@InstallIn(SingletonComponent::class)
-internal interface RepositoryModule {
-
-    @Binds
-    fun bindsPointsRepository(pointRepositoryImpl: PointRepositoryImpl): PointRepository
+val pointRepositoryModule = module {
+    single<PointRepository> {
+        PointRepositoryImpl(get(), get(named(DispatcherType.IO)))
+    }
 }
--- network/point/src/main/java/com/b1nd/dodam/network/point/di/DataSourceModule.kt
+++ network/point/src/main/java/com/b1nd/dodam/network/point/di/DataSourceModule.kt
@@ -6,12 +6,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.dsl.module
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-internal interface DataSourceModule {
-    @Binds
-    @Singleton
-    fun bindsPointDataSource(pointService: PointService): PointDataSource
+val pointDataSourceModule = module {
+    single<PointDataSource> {
+        PointService(get())
+    }
 }
