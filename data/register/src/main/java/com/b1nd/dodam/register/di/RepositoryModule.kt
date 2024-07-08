c82785756d6ee8c884057e69d96d32fd06e05cf5
8954sood <8954sood@naver.com>
2024-07-08 16:59:35 +0900

    style: Apply Spotless Format

--- data/register/src/main/java/com/b1nd/dodam/register/di/RepositoryModule.kt
+++ data/register/src/main/java/com/b1nd/dodam/register/di/RepositoryModule.kt
@@ -3,13 +3,8 @@ package com.b1nd.dodam.register.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.register.repository.RegisterRepository
 import com.b1nd.dodam.register.repositoryImpl.RegisterRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val registerRepositoryModule = module {
     single<RegisterRepository> {
--- network/register/src/main/java/com/b1nd/dodam/register/di/DataSourceModule.kt
+++ network/register/src/main/java/com/b1nd/dodam/register/di/DataSourceModule.kt
@@ -2,12 +2,7 @@ package com.b1nd.dodam.register.di
 
 import com.b1nd.dodam.register.api.RegisterService
 import com.b1nd.dodam.register.datasource.RegisterDataSource
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val registerDataSourceModule = module {
     single<RegisterDataSource> {
