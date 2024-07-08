8af7526d61020694dd2fbb7f1bdda74224f3ae5e
8954sood <8954sood@naver.com>
2024-07-08 14:39:36 +0900

    style: Apply Spotless Format

--- data/point/src/main/java/com/b1nd/dodam/data/point/di/RepositoryModule.kt
+++ data/point/src/main/java/com/b1nd/dodam/data/point/di/RepositoryModule.kt
@@ -3,10 +3,6 @@ package com.b1nd.dodam.data.point.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.point.PointRepository
 import com.b1nd.dodam.data.point.repository.PointRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
--- network/point/src/main/java/com/b1nd/dodam/network/point/di/DataSourceModule.kt
+++ network/point/src/main/java/com/b1nd/dodam/network/point/di/DataSourceModule.kt
@@ -2,12 +2,7 @@ package com.b1nd.dodam.network.point.di
 
 import com.b1nd.dodam.network.point.api.PointService
 import com.b1nd.dodam.network.point.datasource.PointDataSource
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val pointDataSourceModule = module {
     single<PointDataSource> {
