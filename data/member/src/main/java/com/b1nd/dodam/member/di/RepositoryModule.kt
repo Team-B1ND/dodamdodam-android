71a22afbad0abe1c3e71a5ce6a9edd726f773183
8954sood <8954sood@naver.com>
2024-07-09 19:45:28 +0900

    style: Apply Spotless Format

--- data/member/src/main/java/com/b1nd/dodam/member/di/RepositoryModule.kt
+++ data/member/src/main/java/com/b1nd/dodam/member/di/RepositoryModule.kt
@@ -3,10 +3,6 @@ package com.b1nd.dodam.member.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.member.MemberRepository
 import com.b1nd.dodam.member.repository.MemberRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
--- network/member/src/main/java/com/b1nd/dodam/member/di/DataSourceModule.kt
+++ network/member/src/main/java/com/b1nd/dodam/member/di/DataSourceModule.kt
@@ -2,12 +2,7 @@ package com.b1nd.dodam.member.di
 
 import com.b1nd.dodam.member.api.MemberService
 import com.b1nd.dodam.member.datasource.MemberDataSource
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val memberDataSourceModule = module {
     single<MemberDataSource> {
