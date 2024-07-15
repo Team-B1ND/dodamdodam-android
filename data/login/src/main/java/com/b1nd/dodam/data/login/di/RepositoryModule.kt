e790b5fd8567dd6a2b25a207abee86a0fb19af97
8954sood <8954sood@naver.com>
2024-07-15 10:55:15 +0900

    style: Apply Spotless Format

--- data/login/src/main/java/com/b1nd/dodam/data/login/di/RepositoryModule.kt
+++ data/login/src/main/java/com/b1nd/dodam/data/login/di/RepositoryModule.kt
@@ -3,13 +3,8 @@ package com.b1nd.dodam.data.login.di
 import com.b1nd.dodam.common.DispatcherType
 import com.b1nd.dodam.data.login.repository.LoginRepository
 import com.b1nd.dodam.data.login.repositoryimpl.LoginRepositoryImpl
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import javax.inject.Singleton
 
 val loginRepositoryModule = module {
     single<LoginRepository> {
