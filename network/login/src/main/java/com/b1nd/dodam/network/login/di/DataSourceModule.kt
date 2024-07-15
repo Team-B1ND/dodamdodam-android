cf91f1c695eeddd5aa85d1ebef68e0a3cd2f0495
8954sood <8954sood@naver.com>
2024-07-15 10:53:03 +0900

    refactor: Login Di library Hilt to Koin

--- network/login/src/main/java/com/b1nd/dodam/network/login/di/DataSourceModule.kt
+++ network/login/src/main/java/com/b1nd/dodam/network/login/di/DataSourceModule.kt
@@ -6,6 +6,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.dsl.module
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +15,11 @@ internal interface DataSourceModule {
     @Binds
     @Singleton
     fun bindsLoginDataSource(loginService: LoginService): LoginDataSource
+}
+
+val loginDataSourceModule = module {
+
+    single<LoginDataSource> {
+        LoginService(get())
+    }
 }
