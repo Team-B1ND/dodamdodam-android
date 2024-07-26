65e638670a5e6df5160eb0bad43a8938d2725d4c
8954sood <8954sood@naver.com>
2024-07-26 16:52:30 +0900

    chore: Rename Function, File Name And DataStoreFile Rename

--- datastore/src/androidMain/kotlin/com/b1nd/dodam/datastore/DataStoreBuilder.android.kt
+++ datastore/src/androidMain/kotlin/com/b1nd/dodam/datastore/DataStoreBuilder.android.kt
@@ -1,9 +1,8 @@
 package com.b1nd.dodam.datastore
 
 import android.content.Context
-import androidx.datastore.core.DataStore
 
-fun createDataStore(context: Context) = createDataStore(
+fun createDataStore(context: Context) = createCoreDataStore(
     producePath = {
         context.filesDir.resolve(dataStoreFileName).absolutePath
     }
