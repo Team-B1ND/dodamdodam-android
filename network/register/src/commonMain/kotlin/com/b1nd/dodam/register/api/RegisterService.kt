1f8e92f2dc9f9d49fb8da1901fcbc729e4bb429b
8954sood <8954sood@naver.com>
2024-08-02 17:55:12 +0900

    refactor: Network Register Module To Kotlin Multiplatform Module

--- network/register/src/commonMain/kotlin/com/b1nd/dodam/register/api/RegisterService.kt
+++ network/register/src/commonMain/kotlin/com/b1nd/dodam/register/api/RegisterService.kt
@@ -9,6 +9,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 internal class RegisterService(
     private val client: HttpClient,
@@ -17,6 +19,7 @@ internal class RegisterService(
     override suspend fun register(email: String, grade: Int, id: String, name: String, number: Int, phone: String, pw: String, room: Int) {
         return defaultSafeRequest {
             client.post(DodamUrl.Member.REGISTER) {
+                contentType(ContentType.Application.Json)
                 setBody(
                     RegisterRequest(
                         email = email,
--- network/register/src/main/java/com/b1nd/dodam/register/model/RegisterResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.b1nd.dodam.register.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class RegisterResponse(
-    val message: String,
-    val status: Int,
-)
