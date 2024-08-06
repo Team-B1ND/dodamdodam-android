4e54897d2d3fab9117c5408c85c38d3af2b5287c
8954sood <8954sood@naver.com>
2024-08-06 20:55:02 +0900

    style: Apply Spotless Format

--- common/src/iosMain/kotlin/com/b1nd/dodam/common/utiles/Extension.ios.kt
+++ common/src/iosMain/kotlin/com/b1nd/dodam/common/utiles/Extension.ios.kt
@@ -3,5 +3,4 @@ package com.b1nd.dodam.common.utiles
 import platform.Foundation.NSString
 import platform.Foundation.stringWithFormat
 
-actual fun String.Companion.javaFormat(format: String, vararg args: Any?): String =
-    NSString.stringWithFormat(format, args)
+actual fun String.Companion.javaFormat(format: String, vararg args: Any?): String = NSString.stringWithFormat(format, args)
