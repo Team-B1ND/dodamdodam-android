3d3d6f07fca847de425538741af7bbd90c13e19f
8954sood <8954sood@naver.com>
2024-08-12 17:50:55 +0900

    feat: Create Multiplatform Feature, Compose Plugin

---
+++ build-logic/src/main/kotlin/com/b1nd/dodam/convention/MultiplatformFeaturePlugin.kt
@@ -0,0 +1,15 @@
+package com.b1nd.dodam.convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MultiplatformFeaturePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("b1nd.dodam.primitive.multiplatform.compose")
+                apply("b1nd.dodam.primitive.multiplatform.kotlin")
+            }
+        }
+    }
+}
