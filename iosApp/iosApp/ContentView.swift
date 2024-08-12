8a0523f220beae5ddd29df8fd6e91b609262c825
8954sood <8954sood@naver.com>
2024-08-12 16:13:51 +0900

    feat: Connect iOS Compose View

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,19 +2,18 @@ import UIKit
 import SwiftUI
 import DodamTeacher
 
-//struct ComposeView: UIViewControllerRepresentable {
-////    func makeUIViewController(context: Context) -> UIViewController {
-////        MainViewControllerKt.MainViewController()
-////    }
-//
-//    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-//}
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
 
 struct ContentView: View {
     var body: some View {
-//        ComposeView()
-//                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
-        Text("test")
+        ComposeView()
+                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
 
