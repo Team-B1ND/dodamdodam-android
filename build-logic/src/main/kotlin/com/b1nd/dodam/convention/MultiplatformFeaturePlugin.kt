0553d626c096e3116337b771fbc78560df4a71cb
8954sood <8954sood@naver.com>
2024-08-21 23:30:28 +0900

    refactor: Multiplatform Plugins
    각 플러그인 간의 역할을 분리하였습니다.

--- build-logic/src/main/kotlin/com/b1nd/dodam/convention/MultiplatformFeaturePlugin.kt
+++ build-logic/src/main/kotlin/com/b1nd/dodam/convention/MultiplatformFeaturePlugin.kt
@@ -7,6 +7,7 @@ class MultiplatformFeaturePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
+                apply("b1nd.dodam.primitive.multiplatform")
                 apply("b1nd.dodam.primitive.multiplatform.compose")
                 apply("b1nd.dodam.primitive.multiplatform.kotlin")
             }
