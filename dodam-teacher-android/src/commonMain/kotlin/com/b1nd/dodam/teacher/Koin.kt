06b034f7a8549bfd220bcbd0f854132abe8aee25
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-08-28 22:57:46 +0900

    style: Apply Spotless Format

--- dodam-teacher-android/src/commonMain/kotlin/com/b1nd/dodam/teacher/Koin.kt
+++ dodam-teacher-android/src/commonMain/kotlin/com/b1nd/dodam/teacher/Koin.kt
@@ -3,9 +3,9 @@ package com.b1nd.dodam.teacher
 import com.b1nd.dodam.common.network.di.dispatchersModule
 import com.b1nd.dodam.data.login.di.loginRepositoryModule
 import com.b1nd.dodam.datastore.di.dataStoreModule
+import com.b1nd.dodam.login.di.loginViewModelModule
 import com.b1nd.dodam.network.core.di.networkCoreModule
 import com.b1nd.dodam.network.login.di.loginDataSourceModule
-import com.b1nd.dodam.login.di.loginViewModelModule
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 
