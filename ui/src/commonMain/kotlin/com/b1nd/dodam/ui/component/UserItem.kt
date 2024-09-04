71cd87b862898a2dcec91ff1e2946fc07adb3441
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-09-04 16:49:44 +0900

    feat: Create User Item

---
+++ ui/src/commonMain/kotlin/com/b1nd/dodam/ui/component/UserItem.kt
@@ -0,0 +1,94 @@
+package com.b1nd.dodam.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.b1nd.dodam.designsystem.DodamTheme
+import com.b1nd.dodam.ui.icons.DefaultProfile
+import com.b1nd.dodam.ui.icons.DefaultUser
+import com.b1nd.dodam.ui.icons.DodamLogo
+import org.jetbrains.compose.resources.painterResource
+
+
+@Composable
+fun UserItem(
+    modifier: Modifier = Modifier,
+    profileImage: String = "",
+    userName: String,
+    action: @Composable () -> Unit
+){
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp)
+            .background(color = DodamTheme.colors.staticWhite),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ProfileImage(
+            profileImage = profileImage
+        )
+        Text(
+            modifier = Modifier.padding(start = 8.dp),
+            text = userName,
+            style = DodamTheme.typography.headlineMedium(),
+            color = DodamTheme.colors.labelNormal
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        action()
+    }
+}
+
+
+@Composable
+fun ProfileImage(
+    modifier: Modifier = Modifier,
+    profileImage: String,
+) {
+    if (profileImage.isEmpty()) {
+        Box(
+            modifier = modifier
+                .size(40.dp)
+                .background(
+                    color = DodamTheme.colors.fillNormal,
+                    shape = CircleShape,
+                ),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                modifier = Modifier.size(15.dp),
+                contentDescription = "",
+                bitmap = DefaultUser,
+                contentScale = ContentScale.Crop
+            )
+        }
+    } else {
+        AsyncImage(
+            modifier = modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            model = profileImage,
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+        )
+    }
+
+}
