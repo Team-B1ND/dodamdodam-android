c2b68fb4b8d19ace88ea696da6f1246680b883ed
8954sood <8954sood@naver.com>
2024-09-05 12:03:28 +0900

    feat: Create Home Out Card

---
+++ feature-teacher/home/src/commonMain/kotlin/com/b1nd/dodam/home/card/OutCard.kt
@@ -0,0 +1,51 @@
+package com.b1nd.dodam.home.card
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.designsystem.DodamTheme
+import com.b1nd.dodam.designsystem.foundation.DodamIcons
+import com.b1nd.dodam.home.InnerCountCard
+import com.b1nd.dodam.ui.component.DodamContainer
+
+@Composable
+fun OutCard(
+
+) {
+    DodamContainer(
+        icon = DodamIcons.DoorOpen,
+        title = "외출/외박 현황"
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(6.dp)
+                .fillMaxWidth()
+        ) {
+            InnerCountCard(
+                title = "현재 외출중인 학생",
+                content = "13명",
+                buttonText = "12명 대기중",
+                onClick = {
+
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            InnerCountCard(
+                title = "현재 외박중인 학생",
+                content = "10명",
+                buttonText = "11명 대기중",
+                onClick = {
+
+                }
+            )
+        }
+    }
+}
