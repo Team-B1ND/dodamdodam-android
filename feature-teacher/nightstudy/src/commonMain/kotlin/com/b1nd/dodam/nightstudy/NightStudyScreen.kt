0856fa9509501f0c945d0537ed6bffd88cd8a5d7
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-09-10 14:52:25 +0900

    feat: Create Loading

--- feature-teacher/nightstudy/src/commonMain/kotlin/com/b1nd/dodam/nightstudy/NightStudyScreen.kt
+++ feature-teacher/nightstudy/src/commonMain/kotlin/com/b1nd/dodam/nightstudy/NightStudyScreen.kt
@@ -1,19 +1,23 @@
 package com.b1nd.dodam.nightstudy
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +28,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -40,6 +45,7 @@ import com.b1nd.dodam.nightstudy.state.DetailMember
 import com.b1nd.dodam.nightstudy.state.NightStudyUiState
 import com.b1nd.dodam.nightstudy.viewmodel.NightStudyViewModel
 import com.b1nd.dodam.ui.component.DodamMember
+import com.b1nd.dodam.ui.effect.shimmerEffect
 import kotlinx.collections.immutable.toImmutableList
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -240,51 +246,56 @@ fun NightStudyScreen(viewModel: NightStudyViewModel = koinViewModel()) {
                         .clip(shape = RoundedCornerShape(18.dp))
                         .background(DodamTheme.colors.staticWhite),
                 ) {
-                    Text(
-                        text = if (titleIndex == 0) "심자 자습중인 학생" else "심자 대기중인 학생",
-                        color = DodamTheme.colors.labelStrong,
-                        style = DodamTheme.typography.headlineBold(),
-                        modifier = Modifier
-                            .padding(top = 16.dp, start = 16.dp, bottom = 6.dp),
-                    )
-                    LazyColumn(
-                        modifier = Modifier
-                            .padding(10.dp),
-                    ) {
-                        when (val data = if (titleIndex == 0) uiState.nightStudyUiState else uiState.nightStudyPendingUiState) {
-                            is NightStudyUiState.Success -> {
-                                val memberList = data.data
+                    when (val data = if (titleIndex == 0) uiState.nightStudyUiState else uiState.nightStudyPendingUiState) {
+                        is NightStudyUiState.Success -> {
+                            val memberList = data.data
 
-                                var filteredMemberList = if (gradeIndex == 0 && roomIndex == 0) {
-                                    memberList
-                                } else if (gradeIndex == 0 && roomIndex != 0) {
-                                    memberList.filter {
-                                        it?.student?.room == roomIndex
-                                    }
-                                } else if (gradeIndex != 0 && roomIndex == 0) {
-                                    memberList.filter {
-                                        it?.student?.grade == gradeIndex
-                                    }
-                                } else {
-                                    memberList.filter {
-                                        it?.student?.grade == gradeIndex && it.student.room == roomIndex
-                                    }
+                            var filteredMemberList = if (gradeIndex == 0 && roomIndex == 0) {
+                                memberList
+                            } else if (gradeIndex == 0 && roomIndex != 0) {
+                                memberList.filter {
+                                    it?.student?.room == roomIndex
                                 }
-                                if (searchStudent.isNotEmpty()) {
-                                    filteredMemberList = filteredMemberList.filter {
-                                        it?.student?.name?.contains(searchStudent) == true
-                                    }
+                            } else if (gradeIndex != 0 && roomIndex == 0) {
+                                memberList.filter {
+                                    it?.student?.grade == gradeIndex
                                 }
+                            } else {
+                                memberList.filter {
+                                    it?.student?.grade == gradeIndex && it.student.room == roomIndex
+                                }
+                            }
+                            if (searchStudent.isNotEmpty()) {
+                                filteredMemberList = filteredMemberList.filter {
+                                    it?.student?.name?.contains(searchStudent) == true
+                                }
+                            }
+                            Text(
+                                text = if (titleIndex == 0) "심자 자습중인 학생" else "심자 대기중인 학생",
+                                color = DodamTheme.colors.labelStrong,
+                                style = DodamTheme.typography.headlineBold(),
+                                modifier = Modifier
+                                    .padding(top = 16.dp, start = 16.dp, bottom = 6.dp),
+                            )
+                            LazyColumn(
+                                modifier = Modifier
+                                    .padding(horizontal = 10.dp),
+                            ) {
+
+
                                 items(filteredMemberList.size) { listIndex ->
                                     DodamMember(
                                         name = filteredMemberList[listIndex]?.student?.name ?: "",
-                                        modifier = Modifier.padding(bottom = 12.dp),
-                                        icon = null,
+                                        modifier = Modifier
+                                            .padding(bottom = 12.dp),
+                                        icon = null
                                     ) {
                                         val start =
-                                            filteredMemberList[listIndex]?.startAt?.date.toString().split("-")
+                                            filteredMemberList[listIndex]?.startAt?.date.toString()
+                                                .split("-")
                                         val end =
-                                            filteredMemberList[listIndex]?.endAt?.date.toString().split("-")
+                                            filteredMemberList[listIndex]?.endAt?.date.toString()
+                                                .split("-")
 
                                         val a =
                                             if (end[2].toInt() > start[2].toInt()) {
@@ -306,6 +317,7 @@ fun NightStudyScreen(viewModel: NightStudyViewModel = koinViewModel()) {
                                                             end[2].toInt() - 20 - start[2].toInt()
                                                         }
                                                     }
+
                                                     else -> {
                                                         0
                                                     }
@@ -348,11 +360,102 @@ fun NightStudyScreen(viewModel: NightStudyViewModel = koinViewModel()) {
                                     }
                                 }
                             }
+                        }
 
-                            NightStudyUiState.Error -> {}
-                            NightStudyUiState.Loading -> {}
+                        NightStudyUiState.Error -> {}
+                        NightStudyUiState.Loading -> {
+                            Column(
+                                modifier = Modifier
+                                    .size(width = 360.dp, height = 166.dp)
+                            ){
+                                Box(
+                                    modifier = Modifier
+                                        .size(width = 139.dp, height = 32.dp)
+                                        .padding(top = 10.dp, start = 10.dp)
+                                        .background(
+                                            shimmerEffect(),
+                                            RoundedCornerShape(8.dp)
+                                        )
+                                )
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 10.dp)
+                                        .padding(top = 12.dp)
+                                ) {
+                                    Row(
+                                        modifier = Modifier
+                                            .height(48.dp),
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .size(40.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(50.dp)
+                                                )
+                                        )
+                                        Box(
+                                            modifier = Modifier
+                                                .size(width = 71.dp, height = 27.dp)
+                                                .padding(start = 8.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(8.dp)
+                                                )
+                                        )
+                                        Spacer(modifier = Modifier.weight(1f))
+
+                                        Box(
+                                            modifier = Modifier
+                                                .size(width = 71.dp, height = 27.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(8.dp)
+                                                )
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.height(12.dp))
+                                    Row(
+                                        modifier = Modifier
+                                            .height(48.dp),
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Box(
+                                            modifier = Modifier
+                                                .size(40.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(50.dp)
+                                                )
+                                        )
+                                        Box(
+                                            modifier = Modifier
+                                                .size(width = 71.dp, height = 27.dp)
+                                                .padding(start = 8.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(8.dp)
+                                                )
+                                        )
+                                        Spacer(modifier = Modifier.weight(1f))
+
+                                        Box(
+                                            modifier = Modifier
+                                                .size(width = 71.dp, height = 27.dp)
+                                                .background(
+                                                    shimmerEffect(),
+                                                    RoundedCornerShape(8.dp)
+                                                )
+                                        )
+                                    }
+                                }
+
+                            }
                         }
                     }
+
                 }
             }
         }
