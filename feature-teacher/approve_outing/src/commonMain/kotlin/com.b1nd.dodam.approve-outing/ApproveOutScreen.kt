9d929f9c93f05a5d43a8ab51caaa1da4f3bc2a44
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-09-17 00:31:59 +0900

    feat: Create Screen

--- feature-teacher/approve_outing/src/commonMain/kotlin/com.b1nd.dodam.approve-outing/ApproveOutScreen.kt
+++ feature-teacher/approve_outing/src/commonMain/kotlin/com.b1nd.dodam.approve-outing/ApproveOutScreen.kt
@@ -1,10 +1,164 @@
 package com.b1nd.dodam.approve
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import com.b1nd.dodam.designsystem.DodamTheme
+import com.b1nd.dodam.designsystem.component.DodamDefaultTopAppBar
+import com.b1nd.dodam.designsystem.component.DodamSegment
+import com.b1nd.dodam.designsystem.component.DodamSegmentedButton
+import com.b1nd.dodam.designsystem.component.DodamTextField
+import com.b1nd.dodam.designsystem.component.DodamTopAppBar
+import com.b1nd.dodam.designsystem.foundation.DodamIcons
+import com.b1nd.dodam.ui.component.DodamMember
+import kotlinx.collections.immutable.toImmutableList
 
 
 @Composable
 fun ApproveOutScreen() {
-    
+
+    var gradeIndex by remember { mutableIntStateOf(0) }
+    val gradeNumber = listOf(
+        "전체",
+        "1학년",
+        "2학년",
+        "3학년",
+    )
+    val gradeItem = List(4) { index ->
+        DodamSegment(
+            selected = gradeIndex == index,
+            text = gradeNumber[index],
+            onClick = { gradeIndex = index },
+        )
+    }.toImmutableList()
+
+    var roomIndex by remember { mutableIntStateOf(0) }
+    val roomNumber = listOf(
+        "전체",
+        "1반",
+        "2반",
+        "3반",
+        "4반",
+    )
+    val roomItem = List(5) { index ->
+        DodamSegment(
+            selected = roomIndex == index,
+            text = roomNumber[index],
+            onClick = { roomIndex = index },
+        )
+    }.toImmutableList()
+
+    var titleIndex by remember { mutableIntStateOf(0) }
+    val text = listOf(
+        "외출",
+        "외박",
+    )
+    val item = List(2) { index: Int ->
+        DodamSegment(
+            selected = titleIndex == index,
+            text = text[index],
+            onClick = { titleIndex = index },
+        )
+    }.toImmutableList()
+
+    var searchStudent by remember { mutableStateOf("") }
+
+    var selectedItemIndex by remember { mutableStateOf(-1) }
+
+    Scaffold(
+        topBar = {
+            DodamTopAppBar(
+                title = "외출/외박 승인",
+                modifier = Modifier.statusBarsPadding(),
+                onBackClick = {}
+            )
+        },
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DodamTheme.colors.backgroundNormal)
+                .padding(it),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+            ) {
+                Column {
+                    DodamSegmentedButton(
+                        segments = item,
+                        modifier = Modifier.padding(top = 12.dp),
+                    )
+                    DodamSegmentedButton(
+                        segments = gradeItem,
+                        modifier = Modifier.padding(top = 12.dp),
+                    )
+                    DodamSegmentedButton(
+                        segments = roomItem,
+                        modifier = Modifier.padding(top = 12.dp),
+                    )
+                }
+                DodamTextField(
+                    value = searchStudent,
+                    onValueChange = {
+                        searchStudent = it
+                    },
+                    label = "학생 검색",
+                    onClickRemoveRequest = {
+                        searchStudent = ""
+                    },
+                    modifier = Modifier,
+                )
+
+                LazyColumn(
+                    modifier = Modifier.padding(top = 20.dp)
+                ) {
+                    items(3) { index ->
+                        DodamMember(
+                            name = "한준혁",
+                            icon = null,
+                            modifier = Modifier
+                                .padding(bottom = 12.dp)
+                                .clickable {
+                                    selectedItemIndex = index
+                                },
+                            content = {
+                                if (index == selectedItemIndex) {
+                                    Image(
+                                        modifier = Modifier
+                                            .align(Alignment.CenterVertically)
+                                            .size(24.dp),
+                                        imageVector = DodamIcons.CheckmarkCircle.value,
+                                        contentDescription = null,
+                                        colorFilter = ColorFilter.tint(DodamTheme.colors.primaryNormal)
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
