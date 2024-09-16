2a85e06361dd177a404a47da46d486365631b875
8954sood <8954sood@naver.com>
2024-09-16 20:35:44 +0900

    chore: Remove Not Need Premier

--- ui/src/commonMain/kotlin/com/b1nd/dodam/ui/component/modifier/DropShadowModifier.kt
+++ ui/src/commonMain/kotlin/com/b1nd/dodam/ui/component/modifier/DropShadowModifier.kt
@@ -10,12 +10,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-/**
- * Seugi DropShadow
- *
- * @param type: the size of the shadow according to the design system.
- * @param modifier: the Modifier to be applied to this drop shadow
- */
 fun Modifier.dropShadow(blur: Dp, offsetY: Dp, color: Color, modifier: Modifier = Modifier) = then(
     modifier.drawBehind {
         drawIntoCanvas { canvas ->
