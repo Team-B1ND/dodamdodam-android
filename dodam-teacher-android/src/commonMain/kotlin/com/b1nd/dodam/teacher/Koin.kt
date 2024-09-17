e86b696a524bdb83014efeecafa18d67e1f0c590
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-09-17 17:59:41 +0900

    chore: Edit Package

--- dodam-teacher-android/src/commonMain/kotlin/com/b1nd/dodam/teacher/Koin.kt
+++ dodam-teacher-android/src/commonMain/kotlin/com/b1nd/dodam/teacher/Koin.kt
@@ -1,6 +1,6 @@
 package com.b1nd.dodam.teacher
 
-import com.b1nd.dodam.approve.approveOutingViewModelModule
+import com.b1nd.dodam.approveouting.approveOutingViewModelModule
 import com.b1nd.dodam.common.network.di.coroutineScopeModule
 import com.b1nd.dodam.common.network.di.dispatchersModule
 import com.b1nd.dodam.data.banner.di.bannerRepositoryModule
