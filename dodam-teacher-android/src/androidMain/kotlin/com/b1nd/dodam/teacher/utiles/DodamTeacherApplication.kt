41b76082b4b3eb7ec6c280b130f25ffd653fad62
8954sood <8954sood@naver.com>
2024-09-23 17:17:58 +0900

    fix: Invalid init Koin Called Position

--- dodam-teacher-android/src/androidMain/kotlin/com/b1nd/dodam/teacher/utiles/DodamTeacherApplication.kt
+++ dodam-teacher-android/src/androidMain/kotlin/com/b1nd/dodam/teacher/utiles/DodamTeacherApplication.kt
@@ -4,10 +4,27 @@ import android.app.Application
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
+import com.b1nd.dodam.keystore.keystoreManagerModule
 import com.b1nd.dodam.teacher.getAsyncImageLoader
+import com.b1nd.dodam.teacher.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
 
 class DodamTeacherApplication : Application(), SingletonImageLoader.Factory {
     override fun newImageLoader(context: PlatformContext): ImageLoader {
         return getAsyncImageLoader(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin {
+            androidLogger()
+            androidContext(this@DodamTeacherApplication)
+            androidFileProperties()
+            modules(
+                keystoreManagerModule,
+            )
+        }
     }
 }
