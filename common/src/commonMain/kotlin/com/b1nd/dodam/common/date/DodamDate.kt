cfc80eb2fb9e32b848b593022698859be64bdc13
8954sood <8954sood@naver.com>
2024-09-24 11:09:15 +0900

    feat: Create New Ask Out Screen

--- common/src/commonMain/kotlin/com/b1nd/dodam/common/date/DodamDate.kt
+++ common/src/commonMain/kotlin/com/b1nd/dodam/common/date/DodamDate.kt
@@ -2,8 +2,10 @@ package com.b1nd.dodam.common.date
 
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 
 expect object DodamDate {
     fun now(): LocalDateTime
     fun localDateNow(): LocalDate
+    fun localTimeNow(): LocalTime
 }
--- common/src/main/java/com/b1nd/dodam/common/date/DodamDate.android.kt
+++ common/src/main/java/com/b1nd/dodam/common/date/DodamDate.android.kt
@@ -2,10 +2,13 @@ package com.b1nd.dodam.common.date
 
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.toKotlinLocalDate
 import kotlinx.datetime.toKotlinLocalDateTime
+import kotlinx.datetime.toKotlinLocalTime
 
 actual object DodamDate {
     actual fun now(): LocalDateTime = java.time.LocalDateTime.now().toKotlinLocalDateTime()
     actual fun localDateNow(): LocalDate = java.time.LocalDate.now().toKotlinLocalDate()
+    actual fun localTimeNow(): LocalTime = java.time.LocalTime.now().toKotlinLocalTime()
 }
