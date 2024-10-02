360c9e452c96d2d8006025933a15dbdcc85d8dea
8954sood <8954sood@naver.com>
2024-10-02 13:37:22 +0900

    style: Apply Spotless Format

--- feature-student/main/src/main/java/com/b1nd/dodam/student/main/navigation/MainDestination.kt
+++ feature-student/main/src/main/java/com/b1nd/dodam/student/main/navigation/MainDestination.kt
@@ -1,11 +1,5 @@
 package com.b1nd.dodam.student.main.navigation
 
-import androidx.compose.runtime.Composable
-import com.b1nd.dodam.dds.style.DoorOpenIcon
-import com.b1nd.dodam.dds.style.ForkAndKnifeIcon
-import com.b1nd.dodam.dds.style.HomeIcon
-import com.b1nd.dodam.dds.style.MenuIcon
-import com.b1nd.dodam.dds.style.MoonPlusIcon
 import com.b1nd.dodam.designsystem.foundation.DodamIcons
 import com.b1nd.dodam.meal.navigation.MEAL_ROUTE
 import com.b1nd.dodam.member.navigation.ALL_ROUTE
