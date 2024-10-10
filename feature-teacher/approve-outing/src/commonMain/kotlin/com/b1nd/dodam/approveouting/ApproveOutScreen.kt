d8e98bc8d0764dadfab1165b0d454b5e73e6804a
wnsgur1 <asdf1234@dgsw.hs.kr>
2024-10-10 20:10:43 +0900

    feat: Edit Minute Text

--- feature-teacher/approve-outing/src/commonMain/kotlin/com/b1nd/dodam/approveouting/ApproveOutScreen.kt
+++ feature-teacher/approve-outing/src/commonMain/kotlin/com/b1nd/dodam/approveouting/ApproveOutScreen.kt
@@ -351,6 +351,9 @@ fun getTime(time: String): String {
     val date2 = time.split("T")[0].split("-")[2].toInt()
     val hour = time.split("T")[1].split(":")[0].toInt()
     val minute = time.split("T")[1].split(":")[1].toInt()
-
-    return "${date1}월 ${date2}일 $hour:$minute"
+    if (minute < 10){
+        return "${date1}월 ${date2}일 $hour:0${minute}"
+    }else{
+        return "${date1}월 ${date2}일 $hour:$minute"
+    }
 }
