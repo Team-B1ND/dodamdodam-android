19bb05cb3699bed709e86015befa5ecf56ecbaa6
8954sood <8954sood@naver.com>
2024-12-02 09:59:42 +0900

    feat: Add AutoLInk Text and Category Select Logic

--- feature/notice/src/commonMain/kotlin/com/b1nd/dodam/notice/NoticeScreen.kt
+++ feature/notice/src/commonMain/kotlin/com/b1nd/dodam/notice/NoticeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Surface
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,10 +29,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.util.fastForEach
@@ -39,18 +50,22 @@ import coil3.compose.AsyncImage
 import com.b1nd.dodam.designsystem.DodamTheme
 import com.b1nd.dodam.designsystem.animation.rememberBounceIndication
 import com.b1nd.dodam.designsystem.component.ActionIcon
+import com.b1nd.dodam.designsystem.component.DividerType
 import com.b1nd.dodam.designsystem.component.DodamDefaultTopAppBar
+import com.b1nd.dodam.designsystem.component.DodamDivider
 import com.b1nd.dodam.designsystem.foundation.DodamIcons
+import com.b1nd.dodam.ui.component.DodamAutoLinkText
 import com.b1nd.dodam.ui.component.modifier.`if`
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlin.math.max
 import kotlin.math.sign
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun NoticeScreen() {
-
-    val selectCategory by remember { mutableStateOf("전체") }
+    val uriHandler = LocalUriHandler.current
+    var selectCategory by remember { mutableStateOf("전체") }
 
     Scaffold(
         topBar = {
@@ -73,20 +88,27 @@ internal fun NoticeScreen() {
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             stickyHeader {
-                Row(
-                    modifier = Modifier
-                        .horizontalScroll(rememberScrollState())
-                        .background(DodamTheme.colors.backgroundNeutral)
-                        .padding(start = 16.dp, bottom = 1.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    persistentListOf("전체", "B1ND", "CNS", "DUCAMI", "삼디", "모디").fastForEach { category ->
-                        NoticeCategoryCard(
-                            text = category,
-                            isChecked = category == selectCategory,
-                            onClick = {}
-                        )
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .horizontalScroll(rememberScrollState())
+                            .background(DodamTheme.colors.backgroundNeutral)
+                            .padding(start = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        persistentListOf("전체", "B1ND", "CNS", "DUCAMI", "삼디", "모디").fastForEach { category ->
+                            NoticeCategoryCard(
+                                text = category,
+                                isChecked = category == selectCategory,
+                                onClick = {
+                                    selectCategory = category
+                                }
+                            )
+                        }
                     }
+                    DodamDivider(
+                        type = DividerType.Normal
+                    )
                 }
             }
 
@@ -99,7 +121,10 @@ internal fun NoticeScreen() {
                             date = "5월 4일 수요일",
                             title = "취업합시다",
                             content = "얼른",
-                            images = persistentListOf()
+                            images = persistentListOf(),
+                            onLinkClick = { url ->
+                                uriHandler.openUri(url)
+                            }
                         )
                     }
                     1 -> {
@@ -114,7 +139,10 @@ internal fun NoticeScreen() {
                                     "-설문기간: 10/30(수) 13시까지\n" +
                                     "\n" +
                                     "https://forms.gle/wfp1fRNNfcr11RcHA",
-                            images = persistentListOf()
+                            images = persistentListOf(),
+                            onLinkClick = { url ->
+                                uriHandler.openUri(url)
+                            }
                         )
                     }
                     2 -> {
@@ -128,7 +156,10 @@ internal fun NoticeScreen() {
                                 "https://cdn.cvinfo.com/news/photo/202301/24433_27847_5741.jpg",
                                 "https://cdn.cvinfo.com/news/photo/202301/24433_27847_5741.jpg",
                                 "https://cdn.cvinfo.com/news/photo/202301/24433_27847_5741.jpg"
-                            )
+                            ),
+                            onLinkClick = { url ->
+                                uriHandler.openUri(url)
+                            }
                         )
                     }
                 }
@@ -149,6 +180,13 @@ private fun NoticeCategoryCard(
 ) {
     Box(
         modifier = modifier
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberBounceIndication(
+                    radius = RoundedCornerShape(31.dp)
+                ),
+                onClick = onClick
+            )
             .background(
                 color = if (isChecked) DodamTheme.colors.primaryNormal else DodamTheme.colors.backgroundNormal,
                 shape = RoundedCornerShape(31.dp)
@@ -159,12 +197,7 @@ private fun NoticeCategoryCard(
                     color = DodamTheme.colors.lineAlternative,
                     shape = RoundedCornerShape(31.dp)
                 )
-            }
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberBounceIndication(),
-                onClick = onClick
-            ),
+            },
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -186,7 +219,8 @@ private fun NoticeCard(
     date: String,
     title: String,
     content: String,
-    images: ImmutableList<String>
+    images: ImmutableList<String>,
+    onLinkClick: (url: String) -> Unit
 ) {
     Surface(
         modifier = modifier
@@ -211,12 +245,14 @@ private fun NoticeCard(
                 style = DodamTheme.typography.heading2Bold(),
                 color = DodamTheme.colors.labelNormal
             )
-            Text(
+
+            DodamAutoLinkText(
                 text = content,
                 style = DodamTheme.typography.labelRegular().copy(
                     lineHeight = 1.3.em
                 ),
-                color = DodamTheme.colors.labelNormal
+                color = DodamTheme.colors.labelNormal,
+                onLinkClick = onLinkClick
             )
 
             if (images.isNotEmpty()) {
