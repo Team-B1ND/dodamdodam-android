9d44a9c4a9be77e88aa334867029ed2ac59144f4
8954sood <8954sood@naver.com>
2025-02-20 22:01:54 +0900

    fix: Add Change Bottom Visible in Parent Screen

--- feature-parent/main/src/main/java/com/b1nd/dodam/parent/main/ParentMainScreen.kt
+++ feature-parent/main/src/main/java/com/b1nd/dodam/parent/main/ParentMainScreen.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -45,6 +47,7 @@ internal fun ParentMainScreen(
 ) {
     var selectedIndex by rememberSaveable { mutableIntStateOf(0) }
     val mainScreenState = rememberParentMainScreenState(navController)
+    var bottomNavVisible by remember { mutableStateOf(true) }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -59,6 +62,9 @@ internal fun ParentMainScreen(
             noticeScreen(
                 isTeacher = false,
                 navigateToNoticeCreate = null,
+                changeBottomNavVisible = { visible ->
+                    bottomNavVisible = visible
+                }
             )
             parentAllScreen(
                 navigateToSetting = navigateToSetting,
@@ -79,27 +85,30 @@ internal fun ParentMainScreen(
                 ),
         )
 
-        Column(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .navigationBarsPadding()
-                .align(Alignment.BottomCenter),
-        ) {
-            val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+        if (bottomNavVisible) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .navigationBarsPadding()
+                    .align(Alignment.BottomCenter),
+            ) {
+                val currentRoute =
+                    navController.currentBackStackEntryAsState().value?.destination?.route
 
-            DodamNavigationBar(
-                items = mainScreenState.mainDestinations.map {
-                    DodamNavigationBarItem(
-                        selected = currentRoute == it.route,
-                        icon = it.icon,
-                        enable = currentRoute != it.route,
-                        onClick = {
-                            mainScreenState.navigateToMainDestination(it)
-                        },
-                    )
-                }.toImmutableList(),
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+                DodamNavigationBar(
+                    items = mainScreenState.mainDestinations.map {
+                        DodamNavigationBarItem(
+                            selected = currentRoute == it.route,
+                            icon = it.icon,
+                            enable = currentRoute != it.route,
+                            onClick = {
+                                mainScreenState.navigateToMainDestination(it)
+                            },
+                        )
+                    }.toImmutableList(),
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
 }
