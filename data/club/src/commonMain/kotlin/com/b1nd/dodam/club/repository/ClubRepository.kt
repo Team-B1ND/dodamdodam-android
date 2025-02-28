f1afe18947811a2c52eb4e398fbb5c5f97cf52a6
dlrjsgml44 <leegeh1213@gmail.com>
2025-02-28 19:33:54 +0900

    feat: Create Club RepositoryImpl

--- data/club/src/commonMain/kotlin/com/b1nd/dodam/club/repository/ClubRepository.kt
+++ data/club/src/commonMain/kotlin/com/b1nd/dodam/club/repository/ClubRepository.kt
@@ -5,12 +5,13 @@ import com.b1nd.dodam.club.model.ClubJoin
 import com.b1nd.dodam.club.model.ClubMember
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.Flow
+import com.b1nd.dodam.common.result.Result
 
 interface ClubRepository {
     suspend fun postClubJoinRequests(id: Int): Flow<Result<Unit>>
     suspend fun deleteClubJoinRequest(id: Int): Flow<Result<Unit>>
     suspend fun getDetailClub(id: Int): Flow<Result<ClubInfo>>
-    suspend fun getClubJoinRequestReceived(): Flow<Result<ClubJoin>>
+    suspend fun getClubJoinRequestReceived(): Flow<Result<ImmutableList<ClubJoin>>>
     suspend fun getClubLeader(id: Int): Flow<Result<ClubMember>>
     suspend fun getClubAllowMember(id: Int): Flow<Result<ImmutableList<ClubMember>>>
     suspend fun getClubAllMember(id: Int): Flow<Result<ImmutableList<ClubMember>>>
---
+++ data/club/src/commonMain/kotlin/com/b1nd/dodam/club/repositoryimpl/ClubRepositoryImpl.kt
@@ -0,0 +1,100 @@
+package com.b1nd.dodam.club.repositoryimpl
+
+import com.b1nd.dodam.club.datasource.ClubDataSource
+import com.b1nd.dodam.club.model.ClubInfo
+import com.b1nd.dodam.club.model.ClubJoin
+import com.b1nd.dodam.club.model.ClubMember
+import com.b1nd.dodam.club.model.toModel
+import com.b1nd.dodam.club.repository.ClubRepository
+import com.b1nd.dodam.common.Dispatcher
+import com.b1nd.dodam.common.DispatcherType
+import com.b1nd.dodam.common.result.asResult
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import com.b1nd.dodam.common.result.Result
+import kotlinx.collections.immutable.toImmutableList
+
+internal class ClubRepositoryImpl(
+    private val network: ClubDataSource,
+    @Dispatcher(DispatcherType.IO) private val dispatcher: CoroutineDispatcher,
+) : ClubRepository {
+    override suspend fun postClubJoinRequests(id: Int): Flow<Result<Unit>> {
+        return flow {
+            emit(network.postClubJoinRequests(id))
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun deleteClubJoinRequest(id: Int): Flow<Result<Unit>> {
+        return flow {
+            emit(network.deleteClubJoinRequest(id))
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getDetailClub(id: Int): Flow<Result<ClubInfo>> {
+        return flow {
+            emit(
+                network.getDetailClub(id).toModel(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getClubJoinRequestReceived(): Flow<Result<ImmutableList<ClubJoin>>> {
+        return flow {
+            emit(
+                network.getClubJoinRequestReceived().map { it.toModel() }.toImmutableList(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getClubLeader(id: Int): Flow<Result<ClubMember>> {
+        return flow {
+            emit(
+                network.getClubLeader(id).toModel(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getClubAllowMember(id: Int): Flow<Result<ImmutableList<ClubMember>>> {
+        return flow {
+            emit(
+                network.getClubAllowMember(id).map { it.toModel() }.toImmutableList(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getClubAllMember(id: Int): Flow<Result<ImmutableList<ClubMember>>> {
+        return flow {
+            emit(
+                network.getClubAllMember(id).map { it.toModel() }.toImmutableList(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+    override suspend fun getClubList(): Flow<Result<ImmutableList<ClubInfo>>> {
+        return flow {
+            emit(
+                network.getClubList().map { it.toModel() }.toImmutableList(),
+            )
+        }
+            .asResult()
+            .flowOn(dispatcher)
+    }
+
+}
