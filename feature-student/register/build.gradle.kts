05e88274e6b98dd24d72a4a00db3223a2b4ff52d
wnsgur1 <asdf1234@dgsw.hs.kr>
2025-03-03 04:17:30 +0900

    feat: Create SelectRole Screen

--- feature-student/register/build.gradle.kts
+++ feature-student/register/build.gradle.kts
@@ -12,6 +12,7 @@ android {
 
 dependencies {
     implementation(libs.dodam.design.system.cmm)
+    implementation(libs.dodam.design.system)
     implementation(projects.data.register)
     implementation(libs.bottomsheetdialog.compose)
     implementation(projects.common)
