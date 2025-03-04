fe7a13027caa9f969a966e10b147df4d2d08b049
dlrjsgml44 <leegeh1213@gmail.com>
2025-03-04 21:48:57 +0900

    style: Change To Spotless Style

--- data/club/src/commonMain/kotlin/com/b1nd/dodam/club/repository/ClubRepository.kt
+++ data/club/src/commonMain/kotlin/com/b1nd/dodam/club/repository/ClubRepository.kt
@@ -3,16 +3,14 @@ package com.b1nd.dodam.club.repository
 import com.b1nd.dodam.club.model.Club
 import com.b1nd.dodam.club.model.ClubJoin
 import com.b1nd.dodam.club.model.ClubMember
-import com.b1nd.dodam.club.model.ClubResponse
 import com.b1nd.dodam.club.model.ClubState
-import com.b1nd.dodam.club.model.ClubType
 import com.b1nd.dodam.common.result.Result
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.Flow
 
 interface ClubRepository {
     suspend fun postClubJoinRequestsAllow(id: Int): Flow<Result<Unit>>
-    suspend fun postClubJoinRequests(clubId: Int, clubPriority: String, introduce: String) : Flow<Result<Unit>>
+    suspend fun postClubJoinRequests(clubId: Int, clubPriority: String, introduce: String): Flow<Result<Unit>>
     suspend fun deleteClubJoinRequest(id: Int): Flow<Result<Unit>>
     suspend fun getDetailClub(id: Int): Flow<Result<Club>>
     suspend fun getClubJoinRequestReceived(): Flow<Result<ImmutableList<ClubJoin>>>
@@ -22,5 +20,5 @@ interface ClubRepository {
     suspend fun getClubList(): Flow<Result<ImmutableList<Club>>>
     suspend fun getClubJoined(): Flow<Result<ImmutableList<Club>>>
     suspend fun getClubMyCreated(): Flow<Result<ImmutableList<Club>>>
-    suspend fun patchClubState(clubIds: ImmutableList<Int>, status: ClubState, reason: String?) : Flow<Result<Unit>>
+    suspend fun patchClubState(clubIds: ImmutableList<Int>, status: ClubState, reason: String?): Flow<Result<Unit>>
 }
