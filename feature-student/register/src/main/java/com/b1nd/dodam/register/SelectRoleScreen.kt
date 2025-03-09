ef9edaa0010b32b0078128e1f9cf7b3db9fc4ed8
wnsgur1 <asdf1234@dgsw.hs.kr>
2025-03-10 03:44:59 +0900

    style: Apply Spotless Format

--- feature-student/register/src/main/java/com/b1nd/dodam/register/SelectRoleScreen.kt
+++ feature-student/register/src/main/java/com/b1nd/dodam/register/SelectRoleScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
