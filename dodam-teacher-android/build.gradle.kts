36d37862df48031c54f63d50f57fd87a054b9870
8954sood <8954sood@naver.com>
2025-03-09 13:45:24 +0900

    docs: Version Up to s3.5.0 and t3.2.0

--- dodam-teacher-android/build.gradle.kts
+++ dodam-teacher-android/build.gradle.kts
@@ -116,8 +116,8 @@ android {
         applicationId = "com.b1nd.dodam.teacher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.1.0"
+        versionCode = 4
+        versionName = "3.2.0"
 
     }
     compileOptions {
