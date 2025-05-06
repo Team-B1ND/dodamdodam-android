57072a516083decdfbd4aa08a691f75f53113618
dlrjsgml44 <leegeh1213@gmail.com>
2025-05-06 11:57:00 +0900

    feat: Create Night Study Student Model

---
+++ data/night-study/src/commonMain/kotlin/com/b1nd/dodam/data/nightstudy/model/NightStudyStudent.kt
@@ -0,0 +1,26 @@
+package com.b1nd.dodam.data.nightstudy.model
+
+import com.b1nd.dodam.network.nightstudy.model.NightStudyStudentResponse
+
+data class NightStudyStudent(
+    val id: Long,
+    val name: String,
+    val grade: Long,
+    val room: Long,
+    val number: Long,
+    val phone: String,
+    val profileImage: String,
+    val isBanned: Boolean
+)
+
+
+internal fun NightStudyStudentResponse.toModel() : NightStudyStudent = NightStudyStudent(
+    id = id,
+    name = name,
+    grade = grade,
+    room = room,
+    number = number,
+    phone = phone,
+    profileImage = profileImage,
+    isBanned = isBanned
+)
